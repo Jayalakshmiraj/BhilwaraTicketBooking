72b2c420faaa15d4978ba160205bd69ffe73b565
unknown <Jayalakshmiraj@gmail.com>
2019-04-05 09:33:04 +0530

    second

--- src/app/movies.service.ts
+++ src/app/movies.service.ts
@@ -8,13 +8,16 @@ import { Observable} from 'rxjs';
 })
 export default class MoviesService {
   movies=[];
+  
   private movieUrl = 'http://localhost:3000/api/movies';
+  private movie1Url = 'http://localhost:3000/api/booking';
+  
 
   constructor(private http: HttpClient) { }
   getRemoteMovies(): Observable<[any]>{
     return this.http.get<[any]>(this.movieUrl); 		
   }
   getRemoteMovieById(id):Observable<any>{
-    return this.http.get(this.movieUrl + "/" +id);
+    return this.http.get(this.movie1Url + "/" +id);
   }
 }
